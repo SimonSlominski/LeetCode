d8da5bcc71d552bc0a0a53cdcbec9a4217283646
Szymon Słomiński <simon.slominski@gmail.com>
2023-03-05 14:44:43 +0100

    Add 2352 python solution

---
+++ python/2352_Equal Row and Column Pairs.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+
+class Solution(object):
+    def equalPairs(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+
+        def convert_to_key(arr):
+            return tuple(arr)
+
+        dic = defaultdict(int)
+        for row in grid:
+            dic[convert_to_key(row)] += 1
+
+        dic2 = defaultdict(int)
+        for col in range(len(grid[0])):
+            current_col = []
+            for row in range(len(grid)):
+                current_col.append(grid[row][col])
+
+            dic2[convert_to_key(current_col)] += 1
+
+        ans = 0
+        for arr in dic:
+            ans += dic[arr] * dic2[arr]
+
+        return ans
