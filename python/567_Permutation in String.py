afe77b3c10d0633f947e5c44e5fcb4c55d71d833
Szymon Słomiński <simon.slominski@gmail.com>
2023-03-23 18:34:03 +0100

    Add 567 python solution

---
+++ python/567_Permutation in String.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s1) > len(s2):
+            return False
+
+        s1_map, s2_map = [0] * 26, [0] * 26
+
+        for i in range(len(s1)):
+            s1_map[ord(s1[i]) - ord("a")] += 1
+            s2_map[ord(s2[i]) - ord("a")] += 1
+
+        for i in range(len(s2) - len(s1)):
+            if s1_map == s2_map:
+                return True
+
+            s2_map[ord(s2[i]) - ord("a")] -= 1
+            s2_map[ord(s2[i + len(s1)]) - ord("a")] += 1
+
+        return s1_map == s2_map
