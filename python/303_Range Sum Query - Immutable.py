c71727855eef78f875eb19d0bb45f7854a43c72c
Szymon Słomiński <simon.slominski@gmail.com>
2023-03-25 20:43:48 +0100

    Add 303 python solution

---
+++ python/303_Range Sum Query - Immutable.py
@@ -0,0 +1,15 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        return sum(self.nums[left: right + 1])
