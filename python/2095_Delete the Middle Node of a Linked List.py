a111738f4934602d6c580eb5fafeec2258a10390
Szymon Słomiński <simon.slominski@gmail.com>
2023-03-30 21:23:51 +0200

    Add 2095 python solution

---
+++ python/2095_Delete the Middle Node of a Linked List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteMiddle(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = dummy
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Delete the middle node
+        slow.next = slow.next.next
+        return dummy.next
