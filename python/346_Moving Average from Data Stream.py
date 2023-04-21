bb8aa5a75b0cb591cf52093d835955ad426c1059
Szymon Słomiński <simon.slominski@gmail.com>
2023-04-21 10:42:34 +0200

    Add 346 python solution

---
+++ python/346_Moving Average from Data Stream.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+class MovingAverage:
+
+    def __init__(self, size):
+        self.q = deque()
+        self.total = 0
+        self.size = size
+
+    def next(self, val):
+        if len(self.q) == self.size:
+            self.total -= self.q.popleft()
+
+        self.q.append(val)
+        self.total += val
+
+        return self.total / len(self.q)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
