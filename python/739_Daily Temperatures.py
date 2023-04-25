835a4b757c5bc6ad5f646630cf1234b3d0d5da57
Szymon Słomiński <simon.slominski@gmail.com>
2023-04-25 14:46:35 +0200

    Add 739 python solution

---
+++ python/739_Daily Temperatures.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        answer = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                j = stack.pop()
+                answer[j] = i - j
+            stack.append(i)
+
+        return answer
