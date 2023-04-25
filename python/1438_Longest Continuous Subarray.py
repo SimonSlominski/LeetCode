a7d7758e501cb9b636ca717c6b8035e0a9177db9
Szymon Słomiński <simon.slominski@gmail.com>
2023-04-25 14:54:14 +0200

    Add 1438 python solution

---
+++ python/1438_Longest Continuous Subarray.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+
+class Solution(object):
+    def longestSubarray(self, nums, limit):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        increasing = deque()
+        decreasing = deque()
+        left = ans = 0
+
+        for right in range(len(nums)):
+            # maintain the monotonic deques
+            while increasing and increasing[-1] > nums[right]:
+                increasing.pop()
+            while decreasing and decreasing[-1] < nums[right]:
+                decreasing.pop()
+
+            increasing.append(nums[right])
+            decreasing.append(nums[right])
+
+            # maintain window property
+            while decreasing[0] - increasing[0] > limit:
+                if nums[left] == decreasing[0]:
+                    decreasing.popleft()
+                if nums[left] == increasing[0]:
+                    increasing.popleft()
+                left += 1
+
+            ans = max(ans, right - left + 1)
+
+        return ans
